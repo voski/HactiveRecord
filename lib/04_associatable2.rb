09cba871b54a5536d042a41a61b9e6c4e24d1473
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-06 17:12:11 -0800

    Almost done

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,26 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    puts "#{name}".tableize
+    define_method("#{name}") do
+    through_options = self.class.assoc_options[through_name.to_sym]
+    source_options = through_options.model_class.assoc_options[source_name.to_sym]
+
+    select_this = "#{source_options.class_name.pluralize}.*"
+    from_here = "#{through_options.model_class.table_name}"
+    join_this = "#{source_options.class_name.pluralize}"
+
+
+    data = DBConnection.execute(<<-SQL)
+      SELECT
+        #{select_this}
+      FROM
+        #{from_here}
+      JOIN
+        #{join_this} ON #{from_here}.#{through_options.primary_key} = #{source_options.class_name.pluralize}.#{source_options.primary_key}
+    SQL
+
+    self.class.parse_all(data)
+    end
   end
 end
